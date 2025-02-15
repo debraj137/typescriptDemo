57972549eb77388b2f6d46750aa56856f770468e
Debraj Deb <debrajdeb137@gmail.com>
2025-02-15 17:04:22 +0530

    all datatype

--- datatype.js
+++ datatype.js
@@ -1,3 +1,23 @@
-"use strict";
-let lname = 'John';
+var lname = 'John';
 // lname = 10
+var newName = 'debraj';
+console.log(newName.toUpperCase());
+//boolean
+var isValid = true;
+console.log(isValid);
+//array
+var empList;
+empList = ['sachin', 'saurav', 'dravid'];
+var numList = [1, 2, 3, 4, 5];
+var result = numList.filter(function (num) { return num > 2; });
+console.log(result);
+var sum = numList.reduce(function (acc, num) { return acc + num; });
+console.log(sum);
+var d = 3 /* Direction.South */;
+console.log(d);
+//tupple skipped because of less use case in angular
+//any
+var department;
+department = 'it';
+department = 11;
+console.log(department);
--- datatype.ts
+++ datatype.ts
@@ -1,2 +1,28 @@
 let lname = 'John';
 // lname = 10
+let newName:string= 'debraj';
+console.log(newName.toUpperCase());
+//boolean
+let isValid:boolean = true;
+console.log(isValid);
+//array
+let empList: string[];
+empList = ['sachin','saurav','dravid'];
+let numList: Array<number>=[1,2,3,4,5]
+let result = numList.filter((num)=>num>2)
+console.log(result);
+let sum = numList.reduce((acc,num)=>acc + num);
+console.log(sum);
+//enum
+const enum Direction{
+    North, East, West, South
+}
+let d:Direction = Direction.South;
+console.log(d);
+//tupple skipped because of less use case in angular
+//any
+let department:any;
+department = 'it';
+department = 11;
+console.log(department);
+
